b2ba49ee45667482afd90153b5fe702b26e41796
barsbek <mak.bekmamat@gmail.com>
2017-12-23 22:34:33 +0500

    unauthorized user can visit register page

--- src/Private.js
+++ src/Private.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 const Private = (props) => {
-  const inLoginPage = window.location.pathname === "/login";
   if(props.isAuthenticated) {
     return props.children;
   }
-  if(!inLoginPage && !props.loading){
+
+  const currentPath = window.location.pathname;
+  const authPaths = ["/login", "/register"];
+  const authFirst = authPaths.indexOf(currentPath) === -1;
+  if(authFirst && !props.loading){
     return <Redirect to="/login" />;
   }
   return null;
