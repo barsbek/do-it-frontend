565838b270da66b36473d0821891729ebf94af6d
Unknown <mak.bekmamat@gmail.com>
2017-12-30 06:56:51 +0500

    show loading status in collections

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
-import { List, ListItem } from 'material-ui/List';
 import { Link, withRouter } from 'react-router-dom';
+
+import { List, ListItem } from 'material-ui/List';
+import CircularProgress from 'material-ui/CircularProgress';
 import axios from 'axios';
 
 class Collections extends Component {
   constructor() {
     super();
-    this.state = { list: [] };
+    this.state = {
+      loading: true,
+      list: []
+    };
   }
 
   componentWillMount() {
     axios.get('/api/collections')
     .then(res => {
-      this.setState({ list: res.data.collections });
+      this.setState({ list: res.data.collections, loading: false });
     })
-    .catch(err => this.props.notifyErrorResponse(err))
+    .catch(err => {
+      alert(err);
+      this.setState({ loading: false });
+    });
   }
 
   handleItemClick(id) {
     this.props.history.push(`/collections/${id}`)
   }
 
-  render() {
-    const collections = this.state.list.map(c => (
+  renderCollections() {
+    return this.state.list.map(c => (
       <ListItem key={c.id}
         primaryText={c.title}
         secondaryText={c.finish_at}
         onClick={() => this.handleItemClick(c.id)} />
     ));
+  }
 
-    return ( <List>{collections}</List> )
+  render() {
+    if(this.state.loading) return <CircularProgress />
+    return <List>{this.renderCollections()}</List>
   }
 }
 
