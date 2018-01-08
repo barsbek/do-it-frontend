37a32d65a72379db4a02a964ea4cfe34274097bf
Unknown <mak.bekmamat@gmail.com>
2018-01-08 19:07:39 +0500

    remove spare from Collection component

--- src/components/Collection/index.js
+++ src/components/Collection/index.js
@@ -1,40 +1,21 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import axios from 'axios';
-import update from 'immutability-helper';
 
 import CircularProgress from 'material-ui/CircularProgress';
 
-import './index.css';
 import NewListButton from '../List/AddButton';
 import withCrud      from '../hocs/withCrud';
 import withItems     from '../hocs/withItems';
-import Storage       from '../../modules/Storage';
 import SortableLists from './SortableLists';
 
-class Collection extends Component {
-  handleNewList() {
-    this.props.handlers.newItem({ title: '' });
-  }
-
-  render() {
-    return (
-      <div className="collection">
-        <div className="collection-lists">
-          { this.props.loading ?
-            <CircularProgress /> :
-            <SortableLists
-              {...this.props}
-              onSortEnd={this.props.handlers.onSortEnd} />
-          }
-        </div>
-        <NewListButton
-          onClick={this.handleNewList.bind(this)}
-        />
-      </div>
-    );
-  }
-}
+const Collection = props => [
+  (props.loading ? 
+    <CircularProgress /> :
+    <SortableLists {...props} onSortEnd={props.handlers.onSortEnd} />
+  ),
+  <NewListButton
+    onClick={() => props.handlers.newItem({ title: '' })}
+  />
+]
 
 export default withItems({
   pathname: "/api/collections",
